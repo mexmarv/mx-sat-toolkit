1d296f83d3ecebae49cd31bfca8bc1362fb7e6ee
Marvin <mexmarv@gmail.com>
2021-11-20 16:14:05 -0600

    Added ssl library and command to support cert. not verifiable and constructed query directly in soapo body of SUDS client. Added requirements.txt ssl as well.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # Se utiliza FastAPI como framework, Suds-py3 como SOAP client
 # y JSON para serializar
 
-import json
+import json, ssl
 from fastapi import FastAPI, Response
 from suds.client import Client
 
@@ -25,6 +25,9 @@ def fastest_object_to_dict(obj):
             data[field] = fastest_object_to_dict(val)
     return data
 
+#Ignora errores SSL certificados 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 #Inicializa FastAPI
 app = FastAPI(title="SAT México APIs - Toolkit (Marvin Nahmias)",
     description="Aquí encontraras una manera de verificar si una factura o recibo de nómina en México son validos, via un REST API como puente al SAT, sin ´throttling´.",
@@ -37,12 +40,13 @@ async def root():
     return {"Bienvenid@!": "Consulta /docs para mas información de los APIs."}
 
 @app.get("/sat")
-async def sat(rfce: str, rfcr: str, monto: str, folio: str):     
-    obj_sat = {"rfce": rfce, "rfcr": rfcr, "monto": monto, "folio": folio}   
+async def sat(rfce: str, rfcr: str, monto: str, uuid: str):     
+    obj_sat = {"rfce": rfce, "rfcr": rfcr, "monto": monto, "uuid": uuid}   
     
-    if (rfce != "") or (rfcr != "") or (monto != "") or (folio != ""):
-        client = Client('https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc')
-        result = client.service.Consulta(re=rfce, rr=rfcr, tt=monto, id=folio)
+    if (rfce != "") or (rfcr != "") or (monto != "") or (uuid != ""):
+        sat_query = "re=" + rfce + "&rr=" + rfcr + "&tt=" + monto + "&id=" + uuid
+        client = Client('https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc?WDSL')
+        result = client.service.Consulta(sat_query)
         ParsedResponse = fastest_object_to_dict(result)
 
     return Response(content=json.dumps(ParsedResponse),media_type="application/json")
